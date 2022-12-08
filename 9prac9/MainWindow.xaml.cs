9d3b6d0ae484f5420ac8e95d62f59d63533d261a
исп-31 <-31@LAB44-13>
2022-12-08 13:20:30 +0300

    Добавьте файлы проекта.

---
+++ 9prac9/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9prac9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void add(object sender, RoutedEventArgs e)
+            {
+                Int32.TryParse(House.Text, out int house); Int32.TryParse(Flat.Text, out int flat);
+                if (house >= 0 && flat >= 0)
+                {
+                    Students people = new Students(Surname.Text, Street.Text, Convert.ToString(house), Convert.ToString(flat));
+                    dataGrid.Items.Add(people);
+                }
+            }
+        private void tofind(object sender, RoutedEventArgs e)
+        {
+            int kol = 0;
+            foreach (Students people in dataGrid.Items)
+            {
+                if (people.Street == streetsearch.Text)
+                {
+                    kol++;
+                }
+            }
+            MessageBox.Show($"Количество учеников на улице {streetsearch.Text} = {kol}");
+        }
+        private void Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП-31 Кузнецов Алексей Алексеевич. Описать, используя структуру, данные на учеников (фамилия, улица, дом, квартира).Вывести результат на экран. Вывести информацию, сколько учеников живет на заданной улице.");
+        }
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ 9prac9/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _9prac9
+{
+    struct Students
+    {
+        public Students(string surname, string street, string house, string flat)
+        {
+            Surname = surname;
+            Street = street;
+            House = house;
+            Flat = flat;
+        }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Flat { get; set; }
+    }
+}
